0dc54e088edf7075596c281b0af4045cddd5a44b
Raphael Eismann <r.eismann@posteo.de>
2020-10-20 20:48:37 +0000

    update .gitignore; trying oscserver for Controller_Mixer

--- Controller_Mixer/software/src/main.cpp
+++ Controller_Mixer/software/src/main.cpp
@@ -53,7 +53,7 @@
 EthernetUDP Udp;                                    // Create UDP message object
 byte mac[] = {0x04, 0xE9, 0xE5, 0x0C, 0x28, 0xB5};  // teensy's MAC Adress
 IPAddress ip(192, 168, 43, 139);                    // teensy's IP
-const unsigned int inPort  = 8000;                  // Listen port teensy
+const unsigned int inPort  = 8500;                  // Listen port teensy
 
 IPAddress outIp(192, 168, 43, 141);                 // destination IP
 const unsigned int outPort = 9000;                  // Send port teensy
@@ -77,65 +77,22 @@ const int analogInput_4 = 3;            // hc4051 multiplexer
 const int analogInput_5 = 4;            // hc4051 multiplexer
 const int selectPins[3] = {30, 31, 32}; // Multiplexer abc
 
-//////////////////////////////////osc receiver/////////////////////////////////
-//
-// converts the pin to an osc address
-/*
-  char * numToOSCAddress( int pin){
-  static char s[5];
-  int i = 4;
+//////////////////////////////////////OSC Server//////////////////////////////
 
-  s[i--]= '\0';
-  do
-  {
-  s[i] = "01234"[pin % 5];
-  --i;
-  pin /= 5;
-  }
-  while(pin && i);
-  s[i] = '/';
-  return &s[i];
-  }
-
-  void routeLed(OSCMessage &msg, int addrOffset ){
-  //iterate through all the analog pins
-  for(byte pin = 0; pin < 5; pin++){
-  //match against the pin number strings
-  int pinMatched = msg.match(numToOSCAddress(pin), addrOffset);
-  if(pinMatched){
-  unsigned int led_1 = 0;
-  unsigned int led_2 = 0;
-  unsigned int led_3 = 0;
-  unsigned int led_4 = 0;
-  unsigned int led_5 = 0;
-  //if it has an int, then it's an integers frequency in Hz
-  for(int i=0; i<5; i++){ 
-  if (msg.isInt(i)){        
-  led_1 = msg.getInt(i);
-  } 
-  }
-  }
-  }
-  }
+void dispatchAddress_1(OSCMessage &msg){
+    if (msg.isFloat(0)){
+        float val = msg.getFloat(0);
+            Serial.print(val);
+        if (val<=0){
+            digitalWrite(ledPin_1, LOW);
+        }
+        else if (val>=1){
+            digitalWrite(ledPin_1, HIGH);
+        }
+    }
+};
 
 
-  // Button 1
-  buttonState_1 = digitalRead(buttonPin_1);
-  if(buttonState_1 == LOW && x_1 == 1)
-  {
-  digitalWrite(ledPin_1, HIGH);
-  delay(btn_delay);
-  x_1 = 0;
-  Serial.print("btn 1");
-  }
-  else if (buttonState_1 == LOW && x_1 == 0)
-  {
-  digitalWrite(ledPin_1, LOW);
-  delay(btn_delay);
-  x_1 = 1;
-  }
-
-*/
 
 // last sent states
 const int num_tracks = 5;
@@ -145,6 +102,7 @@ int pots_sent[num_tracks][num_pots];
 int buttons_last[num_tracks];
 
 ///////////////////////////////////////Setup///////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 void setup() {
 
@@ -182,11 +140,8 @@ void loop(){
 
 // Jeder Kanalzug am Controller reserviert einen 8-fach Multiplexer (hc4051).
 //
-// Wir haben am Controller 4 Eingangskanäle und den Masterikanal. Die mps sind 
-// über die Stellbits gleichgeschaltet. 
-//
-// getaktet und über die analogPins (siehe oben) 
-// angesclossen.    
+// Wir haben am Controller 4 Eingangskanäle und den Masterikanal. Die Multiplexer 
+// sind über die Stellbits (abc) gleichgeschaltet. 
 
     // OSCBundle bndl;
 
@@ -244,117 +199,41 @@ void loop(){
 
         buttons_last[track] = digital;
     }
-    // bndl.add("/button/1").add((digitalRead(37)==HIGH)?0:1);
-    // bndl.add("/button/2").add((digitalRead(36)==HIGH)?0:1);
-    // bndl.add("/button/3").add((digitalRead(35)==HIGH)?0:1);
-    // bndl.add("/button/4").add((digitalRead(34)==HIGH)?0:1);
-    // bndl.add("/button/5").add((digitalRead(33)==HIGH)?0:1);
 
-
-//////////////////////////////Buttons///////////////////////////////////
+//////////////////////////////Leds//////////////////////////////////
 //
-// Der Toggle zur statusanzeige (osc vom host)
-//
-//
-
-
-
-    /*
     //process received messages
     OSCMessage msg;
-    SLIPEncodedSerial = SLIPSerial(Serial);
-    while(!SLIPSerial.endofPacket()){
-    int size = SLIPSerial.available();
-    if (size > 0){
-    //fill the msg with all of the available bytes
-    while(size--){
-    msg.fill(SLIPSerial.read());
-    }
-    }
-    }
+        int size;
+            if ((size = Udp.parsePacket())>0){
+                //fill the msg with all of the available bytes
+                while(size--){
+                    msg.fill(Udp.read());
+                    msg.dispatch("/track/5/recv/1/volume", dispatchAddress_1);
+                }
+            }
 
-    //returns true if the data in the first position is an integer
-    if (msg.isInt(0)){
-    //get that integer
-    int data = msg.getInt(0);
-    }
-
-    OSCMessage msg("/a/1");
-    msg.dispatch("/a/1", dispatchAddress);
-    
-    //called whenever an OSCMessage's address matches "/a/1"
-    void dispatchAddress(OSCMessage &msg){
-    //do something with the OSCMessage...
-    if (msg.isFloat(0)){
-    float val = msg.getFloat(0);
-    }
-    }
-    */
 /*
-  int btn_delay = 500;
+    void OSCReceive(){
+        OSCBundle   bundleIN;
+        OSCMessage  msgIN;
+        int size;
+        if( (size = Udp.parsePacket())>0) {
+            // if data begins with / it is a message
+            if (udpData[0]==47){
+                msgIN.fill(udpData,size);
+                if(!msgIN.hasError()){
+                    Serial.println("Receiving OSC Message...");
+                    msgIN.route("/track",showMessage);
+                    msgIN.route("/fx",showMessage);
+                    msgIN.route("/fxparam",showMessage);
+                }
+            }
+        }
+    }
+*/
 
-  // Button 1
-  buttonState_1 = digitalRead(buttonPin_1);
-  if(buttonState_1 == LOW && x_1 == 1)
-  {
-  digitalWrite(ledPin_1, HIGH);
-  delay(btn_delay);
-  x_1 = 0;
-  Serial.print("btn 1");
-  }
-  else if (buttonState_1 == LOW && x_1 == 0)
-  {
-  digitalWrite(ledPin_1, LOW);
-  delay(btn_delay);
-  x_1 = 1;
-  }
-
-
-  //Button 2
-  buttonState_2 = digitalRead(buttonPin_2);
-  if(buttonState_2 == LOW && x_2 == 1)
-  {
-  digitalWrite(ledPin_2, HIGH);
-  delay(btn_delay);
-  x_2 = 0;
-  Serial.print("btn 2");
-  }
-  else if (buttonState_2 == LOW && x_2 == 0)
-  {
-  digitalWrite(ledPin_2, LOW);
-  delay(btn_delay);
-  x_2 = 1;
-  }
-  // Button 3
-  buttonState_3 = digitalRead(buttonPin_3);
-  if(buttonState_3 == LOW && x_3 == 1)
-  {
-  digitalWrite(ledPin_3, HIGH);
-  delay(btn_delay);
-  x_3 = 0;
-  Serial.print("btn 3");
-  }
-  else if (buttonState_3 == LOW && x_3 == 0)
-  {
-  digitalWrite(ledPin_3, LOW);
-  delay(btn_delay);
-  x_3 = 1;
-  }
-  // Button 4
-  buttonState_4 = digitalRead(buttonPin_4);
-  if(buttonState_4 == LOW && x_4 == 1)
-  {
-  digitalWrite(ledPin_4, HIGH);
-  delay(btn_delay);
-  x_4 = 0;
-  Serial.print("btn 4");
-  }
-  else if (buttonState_4 == LOW && x_4 == 0)
-  {
-  digitalWrite(ledPin_4, LOW);
-  delay(btn_delay);
-  x_4 = 1;
-  }
+/*
   // Button 5
   buttonState_5 = digitalRead(buttonPin_5);
   if(buttonState_5 == LOW && x_5 == 1)
@@ -369,36 +248,6 @@ void loop(){
   digitalWrite(ledPin_5, LOW);
   delay(btn_delay);
   x_5 = 1;
-  }
-*/
-/*
-  void OSCReceive(){
-  OSCBundle   bundleIN;
-  OSCMessage  msgIN;
-  int size;
-  if( (size = Udp.parsePacket())>0) {
-  byte udpData[size];
-  for (int i=0;i<size;i++) udpData[i]=Udp.read();
-  // if data begins with # it is a bundle
-  if (udpData[0]==35){
-  bundleIN.fill(udpData,size);
-  if(!bundleIN.hasError()) {
-  Serial.println("Receiving OSC Bundle...");
-  bundleIN.route("/track", showMessage);
-  bundleIN.route("/fx", showMessage);
-  bundleIN.route("/fxparam", showMessage);
-  }
-  }
-  // if data begins with / it is a message
-  else if (udpData[0]==47){
-  msgIN.fill(udpData,size);
-  if(!msgIN.hasError()){
-  Serial.println("Receiving OSC Message...");
-  msgIN.route("/track",showMessage);
-  msgIN.route("/fx",showMessage);
-  msgIN.route("/fxparam",showMessage);
-  }
-  }
   }
 */
 }

3e3db2dadc238892cb1b57787f9fa784e42c0d93
Raphael Eismann <r.eismann@posteo.de>
2020-10-21 12:53:01 +0000

    add configblock ip/port

--- Server/osc-router.py
+++ Server/osc-router.py
@@ -6,8 +6,14 @@ from pythonosc import dispatcher
 from pythonosc import osc_server
 from pythonosc.udp_client import SimpleUDPClient
 
-client = SimpleUDPClient('127.0.0.1', 9001)
+# OSC-Server
+RouterIP    = "0.0.0.0"
+RouterPort  = 9000
 
+# OSC-Clients
+reaper      = SimpleUDPClient('127.0.0.1', 9001)
+ctrl_mixer  = SimpleUDPClient('127.0.0.1', 8500)
+ctrl_motion = SimpleUDPClient('127.0.0.1', 8600)
 
 def poti_handler(address: str,
                  *osc_arguments: List[Any]) -> None:
@@ -20,72 +26,72 @@ def poti_handler(address: str,
 
     if track == "1":
         if poti == "1":
-            client.send_message("/track/15/fxeq/hishelf/freq", value)
+            reaper.send_message("/track/15/fxeq/hishelf/freq", value)
         if poti == "2":
             ph_vol = numpy.interp(value,[0,1],[0,0.50])
-            client.send_message("/track/15/fxeq/hishelf/gain", ph_vol)
+            reaper.send_message("/track/15/fxeq/hishelf/gain", ph_vol)
         if poti == "3":
             ph_vol = numpy.interp(value,[0,1],[0.01,0.50])
-            client.send_message("/track/15/fxeq/band/0/gain", ph_vol)
+            reaper.send_message("/track/15/fxeq/band/0/gain", ph_vol)
         if poti == "4":
             ph_vol = numpy.interp(value,[0,1],[0.01,0.50])
-            client.send_message("/track/15/fxeq/loshelf/gain", ph_vol)
+            reaper.send_message("/track/15/fxeq/loshelf/gain", ph_vol)
         if poti == "5":
-            client.send_message("/track/14/volume", value)
+            reaper.send_message("/track/14/volume", value)
     elif track == "2":
         if poti == "1":
-            client.send_message("/track/21/fxeq/hishelf/freq", value)
+            reaper.send_message("/track/21/fxeq/hishelf/freq", value)
         if poti == "2":
             ph_vol = numpy.interp(value,[0,1],[0,0.50])
-            client.send_message("/track/21/fxeq/hishelf/gain", ph_vol)
+            reaper.send_message("/track/21/fxeq/hishelf/gain", ph_vol)
         if poti == "3":
             ph_vol = numpy.interp(value,[0,1],[0.01,0.50])
-            client.send_message("/track/21/fxeq/band/0/gain", ph_vol)
+            reaper.send_message("/track/21/fxeq/band/0/gain", ph_vol)
         if poti == "4":
             ph_vol = numpy.interp(value,[0,1],[0.01,0.50])
-            client.send_message("/track/21/fxeq/loshelf/gain", ph_vol)
+            reaper.send_message("/track/21/fxeq/loshelf/gain", ph_vol)
         if poti == "5":
-            client.send_message("/track/20/volume", value)
+            reaper.send_message("/track/20/volume", value)
     elif track == "3":
         if poti == "1":
-            client.send_message("/track/27/fxeq/hishelf/freq", value)
+            reaper.send_message("/track/27/fxeq/hishelf/freq", value)
         if poti == "2":
             ph_vol = numpy.interp(value,[0,1],[0,0.50])
-            client.send_message("/track/27/fxeq/hishelf/gain", ph_vol)
+            reaper.send_message("/track/27/fxeq/hishelf/gain", ph_vol)
         if poti == "3":
             ph_vol = numpy.interp(value,[0,1],[0.01,0.50])
-            client.send_message("/track/27/fxeq/band/0/gain", ph_vol)
+            reaper.send_message("/track/27/fxeq/band/0/gain", ph_vol)
         if poti == "4":
             ph_vol = numpy.interp(value,[0,1],[0.01,0.50])
-            client.send_message("/track/27/fxeq/loshelf/gain", ph_vol)
+            reaper.send_message("/track/27/fxeq/loshelf/gain", ph_vol)
         if poti == "5":
-            client.send_message("/track/26/volume", value)
+            reaper.send_message("/track/26/volume", value)
     elif track == "4":
         if poti == "1":
-            client.send_message("/track/33/fxeq/hishelf/freq", value)
+            reaper.send_message("/track/33/fxeq/hishelf/freq", value)
         if poti == "2":
             ph_vol = numpy.interp(value,[0,1],[0,0.50])
-            client.send_message("/track/33/fxeq/hishelf/gain", ph_vol)
+            reaper.send_message("/track/33/fxeq/hishelf/gain", ph_vol)
         if poti == "3":
             ph_vol = numpy.interp(value,[0,1],[0.01,0.50])
-            client.send_message("/track/33/fxeq/band/0/gain", ph_vol)
+            reaper.send_message("/track/33/fxeq/band/0/gain", ph_vol)
         if poti == "4":
             ph_vol = numpy.interp(value,[0,1],[0.01,0.50])
-            client.send_message("/track/33/fxeq/loshelf/gain", ph_vol)
+            reaper.send_message("/track/33/fxeq/loshelf/gain", ph_vol)
         if poti == "5":
-            client.send_message("/track/32/volume", value)
+            reaper.send_message("/track/32/volume", value)
     elif  track == "5":
         if poti == "1":
-            client.send_message("/master/volume", value)
+            reaper.send_message("/master/volume", value)
         if poti == "2":
             ph_vol = numpy.interp(value,[0,1],[0.01,0.5])
-            client.send_message("/track/7/fxeq/hishelf/gain", ph_vol)
+            reaper.send_message("/track/7/fxeq/hishelf/gain", ph_vol)
         if poti == "3":
             ph_vol = numpy.interp(value,[0,1],[0.01,0.5])
-            client.send_message("/track/7/fxeq/band/0/gain", ph_vol)
+            reaper.send_message("/track/7/fxeq/band/0/gain", ph_vol)
         if poti == "4":
             ph_vol = numpy.interp(value,[0,1],[0.01,0.5])
-            client.send_message("/track/7/fxeq/loshelf/gain", ph_vol)
+            reaper.send_message("/track/7/fxeq/loshelf/gain", ph_vol)
 
 def button_handler(address: str,
                    *osc_arguments: List[Any]) -> None:
@@ -94,66 +100,66 @@ def button_handler(address: str,
 
     value = osc_arguments[0]
     if button == "1":
-        client.send_message("/track/2/volume", 0)
-        client.send_message("/track/18/send/1/volume", 1) #dj1 m/s
-        client.send_message("/track/19/send/1/volume", 1) #dj1 b-format
-        client.send_message("/track/24/send/1/volume", 0) #dj2 ..
-        client.send_message("/track/25/send/1/volume", 0) #..
-        client.send_message("/track/30/send/1/volume", 0)
-        client.send_message("/track/31/send/1/volume", 0)
-        client.send_message("/track/36/send/1/volume", 0)
-        client.send_message("/track/37/send/1/volume", 0)
+        reaper.send_message("/track/2/volume", 0)
+        reaper.send_message("/track/18/send/1/volume", 1) #dj1 m/s
+        reaper.send_message("/track/19/send/1/volume", 1) #dj1 b-format
+        reaper.send_message("/track/24/send/1/volume", 0) #dj2 ..
+        reaper.send_message("/track/25/send/1/volume", 0) #..
+        reaper.send_message("/track/30/send/1/volume", 0)
+        reaper.send_message("/track/31/send/1/volume", 0)
+        reaper.send_message("/track/36/send/1/volume", 0)
+        reaper.send_message("/track/37/send/1/volume", 0)
 
     if button == "2":
-        client.send_message("/track/2/volume", 0)
-        client.send_message("/track/18/send/1/volume", 0) #dj1 m/s
-        client.send_message("/track/19/send/1/volume", 0) #dj1 b-format
-        client.send_message("/track/24/send/1/volume", 1) #dj2 ..
-        client.send_message("/track/25/send/1/volume", 1) #..
-        client.send_message("/track/30/send/1/volume", 0)
-        client.send_message("/track/31/send/1/volume", 0)
-        client.send_message("/track/36/send/1/volume", 0)
-        client.send_message("/track/37/send/1/volume", 0)
+        reaper.send_message("/track/2/volume", 0)
+        reaper.send_message("/track/18/send/1/volume", 0) #dj1 m/s
+        reaper.send_message("/track/19/send/1/volume", 0) #dj1 b-format
+        reaper.send_message("/track/24/send/1/volume", 1) #dj2 ..
+        reaper.send_message("/track/25/send/1/volume", 1) #..
+        reaper.send_message("/track/30/send/1/volume", 0)
+        reaper.send_message("/track/31/send/1/volume", 0)
+        reaper.send_message("/track/36/send/1/volume", 0)
+        reaper.send_message("/track/37/send/1/volume", 0)
 
     if button == "3":
-        client.send_message("/track/2/volume", 0)
-        client.send_message("/track/18/send/1/volume", 0) #dj1 m/s
-        client.send_message("/track/19/send/1/volume", 0) #dj1 b-format
-        client.send_message("/track/24/send/1/volume", 0) #dj2 ..
-        client.send_message("/track/25/send/1/volume", 0) #..
-        client.send_message("/track/30/send/1/volume", 1)
-        client.send_message("/track/31/send/1/volume", 1)
-        client.send_message("/track/36/send/1/volume", 0)
-        client.send_message("/track/37/send/1/volume", 0)
+        reaper.send_message("/track/2/volume", 0)
+        reaper.send_message("/track/18/send/1/volume", 0) #dj1 m/s
+        reaper.send_message("/track/19/send/1/volume", 0) #dj1 b-format
+        reaper.send_message("/track/24/send/1/volume", 0) #dj2 ..
+        reaper.send_message("/track/25/send/1/volume", 0) #..
+        reaper.send_message("/track/30/send/1/volume", 1)
+        reaper.send_message("/track/31/send/1/volume", 1)
+        reaper.send_message("/track/36/send/1/volume", 0)
+        reaper.send_message("/track/37/send/1/volume", 0)
 
     if button == "4":
-        client.send_message("/track/2/volume", 0)
-        client.send_message("/track/18/send/1/volume", 0) #dj1 m/s
-        client.send_message("/track/19/send/1/volume", 0) #dj1 b-format
-        client.send_message("/track/24/send/1/volume", 0) #dj2 ..
-        client.send_message("/track/25/send/1/volume", 0) #..
-        client.send_message("/track/30/send/1/volume", 0)
-        client.send_message("/track/31/send/1/volume", 0)
-        client.send_message("/track/36/send/1/volume", 1)
-        client.send_message("/track/37/send/1/volume", 1)
+        reaper.send_message("/track/2/volume", 0)
+        reaper.send_message("/track/18/send/1/volume", 0) #dj1 m/s
+        reaper.send_message("/track/19/send/1/volume", 0) #dj1 b-format
+        reaper.send_message("/track/24/send/1/volume", 0) #dj2 ..
+        reaper.send_message("/track/25/send/1/volume", 0) #..
+        reaper.send_message("/track/30/send/1/volume", 0)
+        reaper.send_message("/track/31/send/1/volume", 0)
+        reaper.send_message("/track/36/send/1/volume", 1)
+        reaper.send_message("/track/37/send/1/volume", 1)
 
     if button == "5":
-        client.send_message("/track/2/volume", 1)
-        client.send_message("/track/18/send/1/volume", 0)
-        client.send_message("/track/19/send/1/volume", 0)
-        client.send_message("/track/24/send/1/volume", 0)
-        client.send_message("/track/25/send/1/volume", 0)
-        client.send_message("/track/30/send/1/volume", 0)
-        client.send_message("/track/31/send/1/volume", 0)
-        client.send_message("/track/36/send/1/volume", 0)
-        client.send_message("/track/37/send/1/volume", 0)
+        reaper.send_message("/track/2/volume", 1)
+        reaper.send_message("/track/18/send/1/volume", 0)
+        reaper.send_message("/track/19/send/1/volume", 0)
+        reaper.send_message("/track/24/send/1/volume", 0)
+        reaper.send_message("/track/25/send/1/volume", 0)
+        reaper.send_message("/track/30/send/1/volume", 0)
+        reaper.send_message("/track/31/send/1/volume", 0)
+        reaper.send_message("/track/36/send/1/volume", 0)
+        reaper.send_message("/track/37/send/1/volume", 0)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip",
-                        default="0.0.0.0", help="The ip to listen on")
+                        default=RouterIP, help="The ip to listen on")
     parser.add_argument("--port",
-                        type=int, default=9000, help="The port to listen on")
+                        type=int, default=RouterPort, help="The port to listen on")
     args = parser.parse_args()
 
     dispatcher = dispatcher.Dispatcher()

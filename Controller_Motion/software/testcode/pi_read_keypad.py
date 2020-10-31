191460d93642ebd4b72932c1983f8fc822796c25
Disc0erg0sum <jendrik@freezeframes.de>
2020-10-31 20:59:46 +0100

    test read in

--- Controller_Motion/software/testcode/pi_read_keypad.py
+++ Controller_Motion/software/testcode/pi_read_keypad.py
@@ -1,81 +1,54 @@
 #!/usr/bin/python3
+"""
+Read out the Keypad
+
+the key are conect to the pi with a 74CH4051
+s0 ----- GPIO_22
+s1 ----- GPIO_27
+s2 ----- GPIO_17
+MUX1_z--- GPIO_04
+MUX2_z--- GPIO_05
+
+"""
 import RPi.GPIO as gpio
-import time
-# Import needed modules from osc4py3
-from osc4py3.as_eventloop import *
-from osc4py3 import oscbuildparse
+import numpy as np
+from time import sleep
 
-
-# Start the system.
-osc_startup()
-
-# Make client channels to send packets.
-osc_udp_client("192.168.43.129", 9000, "aclientname")
+# var
+s0 = 22
+s1 = 27
+s2 = 17
+mux1 = 4  # z
+mux2 = 5  # z
+mux1_out = [0, 1, 2, 3, 4, 5, 6, 7]
 
 gpio.setmode(gpio.BCM)
 
-matrix = [["1ch1","1ch2","1ch3", "1ch4"],
-    ["2ch1", "2ch2", "2ch3", "2ch4"],
-    ["3ch1", "3ch2", "3ch3", "3ch4"],
-    ["4ch1", "4ch2", "4ch3", "4ch4"]]
+# setup PIN's
+gpio.setup(s0, gpio.OUT)
+gpio.setup(s1, gpio.OUT)
+gpio.setup(s2, gpio.OUT)
+gpio.setup(mux1, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(mux2, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
-#spalte = [4, 17, 27, 22]
-#zeile = [5, 6, 13, 19]
-spalte = [5, 6, 13, 19]
-zeile = [4, 17, 27, 22]
 
-for j in range(4):
-    gpio.setup(spalte[j], gpio.OUT)
-    gpio.output(spalte[j], 1)
+def read_mux_pin(z, pin):
+    """
+    docstring
+    """
+    bit_addr = np.binary_repr(pin, width=3)
+    gpio.output(s0, int(bit_addr[0]))
+    gpio.output(s1, int(bit_addr[1]))
+    gpio.output(s2, int(bit_addr[2]))
+    return gpio.input(z)
 
-for i in range(4):
-    gpio.setup(zeile[i], gpio.IN, pull_up_down = gpio.PUD_UP)
 
-def keypad_press():
-    while True:
-        for j in range(4):
-            gpio.output(spalte[j], 0)
-            for i in range(4):
-                if gpio.input(zeile[i]) == 0:
-                    benutzerEingabe = matrix[i][j]
-                    while gpio.input(zeile[i]) == 0:
-
-                        pass
-                    return benutzerEingabe                  
-            gpio.output(spalte[j], 1)
-    return False
-
-def keypad_release():
-    while True:
-        for j in range(4):
-            gpio.output(spalte[j], 0)
-            for i in range(4):
-                if gpio.input(zeile[i]) == 1:
-                    benutzerEingabe = matrix[i][j]
-                    while gpio.input(zeile[i]) == 1:
-
-                        pass
-                    return benutzerEingabe                  
-            gpio.output(spalte[j], 1)
-    return False
-
-try:
-    while True:
-
-        addr = "/Button/" + keypad_press()
-        msg = oscbuildparse.OSCMessage(addr, ",i", [1])
-        osc_send(msg, "aclientname")
-
-        
-        # if keypad_press() == "1ch2":
-        #     print("1ch2")
-        #     # Build a simple message and send it.
-        #     msg = oscbuildparse.OSCMessage("/test/me", ",sif", ["button2", 1, 2])
-        #     osc_send(msg, "aclientname")
-        
-        osc_process()
-        time.sleep(0.1)
-
-except KeyboardInterrupt:
-    gpio.cleanup()
-    osc_terminate()
+if __name__ == "__main__":
+    try:
+        while True:
+            for i in range(8):
+                mux1_out[i] = read_mux_pin(mux1, i)
+            print(mux1_out)
+        sleep(1/35)
+    except KeyboardInterrupt:
+        gpio.cleanup()

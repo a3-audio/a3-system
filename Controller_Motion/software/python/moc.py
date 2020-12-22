97b404dbec6108523158521c3c5787591029dcf6
Raphael Eismann <r.eismann@posteo.de>
2020-12-22 19:23:23 +0000

    init moc.py

---
+++ Controller_Motion/software/python/moc.py
@@ -0,0 +1,73 @@
+
+# potis
+width_1 = 0
+width_2 = 0
+width_3 = 0
+width_4 = 0
+
+reverb_1 = 0
+reverb_2 = 0
+reverb_3 = 0
+reverb_4 = 0
+
+# Encoder
+encoder_1 = 0
+encoder_2 = 0
+encoder_3 = 0
+encoder_4 = 0
+
+# Encoder Buttons
+encoderBtn_1 = 0
+encoderBtn_2 = 0
+encoderBtn_3 = 0
+encoderBtn_4 = 0
+
+# Buttonmatrix
+Button_1 = 0
+Button_2 = 0
+Button_3 = 0
+Button_4 = 0
+Button_5 = 0
+Button_6 = 0
+Button_7 = 0
+Button_8 = 0
+Button_9 = 0
+Button_10 = 0
+Button_11 = 0
+Button_12 = 0
+Button_13 = 0
+Button_14 = 0
+Button_15 = 0
+Button_16 = 0
+
+# Nanopixel
+pix_1 = 0
+pix_2 = 0
+pix_3 = 0
+pix_4 = 0
+pix_5 = 0
+pix_6 = 0
+pix_7 = 0
+pix_8 = 0
+pix_9 = 0
+pix_10 = 0
+pix_11 = 0
+pix_12 = 0
+pix_13 = 0
+pix_14 = 0
+pix_15 = 0
+pix_16 = 0
+
+# Touchscreen
+inTouch_xy = 0, 0
+
+def set_Record_Length()
+def record_touch_to_File()
+def play_from_File(play, pause)
+
+def set_Width()
+def set_Reverb()
+def set_mode(mono, stereo, ambisonic)
+
+
+

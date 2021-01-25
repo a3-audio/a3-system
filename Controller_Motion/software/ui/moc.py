f7699979fb22072e1ea4b31aff71ae3bd0bbbc36
Patric Schmitz <bzk0711@aol.com>
2021-01-25 20:32:15 +0100

    add --develop mode to run without mockup ui in fullscreen

--- Controller_Motion/software/ui/moc.py
+++ Controller_Motion/software/ui/moc.py
@@ -1,8 +1,11 @@
 import sys
+import argparse
+
 from PySide6.QtCore import QFile, QIODevice
 from PySide6.QtCore import QSize
 from PySide6.QtWidgets import QDial
 from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QMainWindow
 from PySide6.QtUiTools import QUiLoader
 
 from widgets.QuadraticDial import QuadraticDial
@@ -10,29 +13,42 @@ from widgets.QuadraticPushButton import QuadraticPushButton
 from widgets.MotionControllerDisplay import MotionControllerDisplay
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='AAA Motion Controller.')
+    parser.add_argument("--develop", help="run in development mode with mockup UI", action="store_true")
+    args = parser.parse_args()
+    if args.develop:
+        print("running in develop mode")
+    else:
+        print("running in fullscreen mode")
+
     app = QApplication(sys.argv)
 
-    ui_file_name = "moc.ui"
-    ui_file = QFile(ui_file_name)
-    if not ui_file.open(QIODevice.ReadOnly):
-        print("Cannot open {}: {}".format(ui_file_name, ui_file.errorString()))
-        sys.exit(-1)
-    loader = QUiLoader()
-    loader.registerCustomWidget(QuadraticDial)
-    loader.registerCustomWidget(QuadraticPushButton)
-    loader.registerCustomWidget(MotionControllerDisplay)
+    if args.develop:
+        ui_file_name = "moc.ui"
+        ui_file = QFile(ui_file_name)
+        if not ui_file.open(QIODevice.ReadOnly):
+            print("Cannot open {}: {}".format(ui_file_name, ui_file.errorString()))
+            sys.exit(-1)
+        loader = QUiLoader()
+        loader.registerCustomWidget(QuadraticDial)
+        loader.registerCustomWidget(QuadraticPushButton)
+        loader.registerCustomWidget(MotionControllerDisplay)
 
-    print(loader.availableWidgets())
+        window = loader.load(ui_file)
+        ui_file.close()
+        if not window:
+            print(loader.errorString())
+            sys.exit(-1)
 
-    window = loader.load(ui_file)
-    ui_file.close()
-    if not window:
-        print(loader.errorString())
-        sys.exit(-1)
+        window.setFixedSize(318, 1000)
+        window.show()
+    else:
+        window = QMainWindow()
+        window.setCentralWidget(MotionControllerDisplay())
+        window.showFullScreen()
 
     # create track objects and pass to display widget
-
-    window.setFixedSize(318, 1000)
-    window.show()
+    # num_tracks = 4
+    # tracks = [Track() for i in range(num_tracks)]
 
     sys.exit(app.exec_())

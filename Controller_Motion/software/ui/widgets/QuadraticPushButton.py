7abbba054971bb87f64f8e4f72f0fd82657f2e51
Patric Schmitz <bzk0711@aol.com>
2021-02-02 12:00:34 +0100

    add MotionControllerUI as submodule

--- Controller_Motion/software/ui/widgets/QuadraticPushButton.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtCore import QSize
-from PySide6.QtWidgets import QPushButton
-
-class QuadraticPushButton(QPushButton):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-    def sizeHint(self):
-        return QSize(64, 64)

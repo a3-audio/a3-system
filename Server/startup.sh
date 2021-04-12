23d15a2cfb281101220dfd71d1a7e9de1ff3aa10
Raphael <r.eismann@posteo.de>
2021-04-12 20:13:24 +0200

    vu-meter.scd to autostart on aaa-core

--- Server/startup.sh
+++ Server/startup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 python /home/aaa/ambijockey/Server/server.py &
-#python /home/aaa/ambijockey/Controller_Mixer/software/python/mic.py &
-
+/usr/bin/sclang /home/aaa/Ambijockey/Server/supercollider/vu-meter.scd &

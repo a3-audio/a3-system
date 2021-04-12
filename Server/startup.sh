212fc56bf0ed91e9648aab3dd33902b8ee8a0d97
Raphael <r.eismann@posteo.de>
2021-04-12 19:52:03 +0200

    vu.py trashed

--- Server/startup.sh
+++ Server/startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 python /home/aaa/ambijockey/Server/server.py &
-python /home/aaa/ambijockey/Controller_Mixer/software/python/mic.py &
+#python /home/aaa/ambijockey/Controller_Mixer/software/python/mic.py &
+

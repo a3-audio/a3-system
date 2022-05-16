b2e00e38bb473b9305f58d236ba4d71a88ce5f59
Raphael Eismann <r.eismann@posteo.de>
2022-05-16 18:25:47 +0200

    license ready

--- a3core/a3_osc_router.py
+++ a3core/a3_osc_router.py
@@ -1,17 +1,15 @@
-# This file is part of A³Pandemic.
-
-# A³Pandemic is free software: you can redistribute it and/or modify
+# A³ Core is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# A³Pandemic is distributed in the hope that it will be useful,
+# A³ Core is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with A³Pandemic.  If not, see <https://www.gnu.org/licenses/>.
+# along with A³ Core.  If not, see <https://www.gnu.org/licenses/>.
 
 # © Copyright 2021 Raphael Eismann, Patric Schmitz
 

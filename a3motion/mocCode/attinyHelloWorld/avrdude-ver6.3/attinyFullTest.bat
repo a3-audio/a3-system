cd436844bfff0fc5e494ba1e75a6c63598c2eb06
flexbex <felix.dahmen@gmail.com>
2023-01-14 19:12:18 +0800

    clean up

--- a3motion/mocCode/attinyHelloWorld/avrdude-ver6.3/attinyFullTest.bat
+++
@@ -1,7 +0,0 @@
-cp C:\Users\tiri\Documents\GitHub\a3-system\a3motion\mocCode\attinyFullTest\attinyFullTest\attinyFullTest.X\dist\Release\production\attinyFullTest.X.production.hex C:\Users\tiri\Documents\GitHub\a3-system\a3motion\mocCode\attinyHelloWorld\avrdude-ver6.3\attinyFullTest.hex
-
-cd C:\Users\tiri\Documents\GitHub\a3-system\a3motion\mocCode\attinyHelloWorld\avrdude-ver6.3\
-
-avrdude.exe -c jtag2updi -P com7 -p t1626 -U flash:w:attinyFullTest.hex
-
-
